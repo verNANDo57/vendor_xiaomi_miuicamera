259ec2fb645161aace2808c035aa2860ca086e22
verNANDo57 <silvenation@gmail.com>
2021-08-28 22:15:22 +0700

    MiuiCamera: revert commit cf43395381d4df5be8e52762354e0338e5e57462

--- config.mk
+++ config.mk
@@ -4,31 +4,13 @@ PRODUCT_SOONG_NAMESPACES += \
     $(CAMERAPATCH_PATH)
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc/default-permissions,$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions) \
-    $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc/permissions,$(TARGET_COPY_OUT_SYSTEM)/etc/permissions) \
-    $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc/sysconfig,$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig) \
-    $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc/ANXCamera/fonts,$(TARGET_COPY_OUT_SYSTEM)/etc/ANXCamera/fonts) \
-    $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc/ANXCamera/cheatcodes,$(TARGET_COPY_OUT_SYSTEM)/etc/ANXCamera/cheatcodes) \
+    $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
     $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
     $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
     $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
     $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
     $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/vendor/camera,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/camera) \
     $(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system_ext/lib,$(TARGET_COPY_OUT_SYSTEM_EXT)/lib)
-
-ifneq ($(filter joyeuse,$(TARGET_DEVICE)),)
-	PRODUCT_COPY_FILES += \
-		$(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc/ANXCamera/features/joyeuse.xml,$(TARGET_COPY_OUT_SYSTEM)/etc/ANXCamera/features/joyeuse.xml)
-else ifneq ($(filter curtana,$(TARGET_DEVICE)),)
-	PRODUCT_COPY_FILES += \
-		$(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc/ANXCamera/features/curtana.xml,$(TARGET_COPY_OUT_SYSTEM)/etc/ANXCamera/features/curtana.xml)
-else ifneq ($(filter gram,$(TARGET_DEVICE)),)
-	PRODUCT_COPY_FILES += \
-		$(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc/ANXCamera/features/gram.xml,$(TARGET_COPY_OUT_SYSTEM)/etc/ANXCamera/features/gram.xml)
-else ifneq ($(filter excalibur,$(TARGET_DEVICE)),)
-	PRODUCT_COPY_FILES += \
-		$(call find-copy-subdir-files,*,$(CAMERAPATCH_PATH)/proprietary/system/etc/ANXCamera/features/excalibur.xml,$(TARGET_COPY_OUT_SYSTEM)/etc/ANXCamera/features/excalibur.xml)
-endif
 
 PRODUCT_PACKAGES += \
     MiuiCamera \
